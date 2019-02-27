2087652c5864ab382d98b27f2543e0b8af9b3168
thestacyharris <40574323+thestacyharris@users.noreply.github.com>
2019-02-27 17:24:51 -0500

    Add files via upload

---
+++ Assignment10/Assignment10/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Assignment10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.assignTenTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // assignTenTextBox
+            // 
+            this.assignTenTextBox.Location = new System.Drawing.Point(133, 53);
+            this.assignTenTextBox.Name = "assignTenTextBox";
+            this.assignTenTextBox.Size = new System.Drawing.Size(100, 20);
+            this.assignTenTextBox.TabIndex = 0;
+            this.assignTenTextBox.TextChanged += new System.EventHandler(this.assignTenTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter a letter (A-Za-z), number (0-9), or special symbol (#@!$%^&*)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 256);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.assignTenTextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assignment 10";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox assignTenTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assignment10/Assignment10/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void assignTenTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string userInput = assignTenTextBox.Text;
+
+            if (assignTenTextBox.Text == "exit")
+            {
+                MessageBox.Show("Please sir, can I have some more...input??");
+                Close();
+            }
+
+            if (string.IsNullOrWhiteSpace(userInput) == false)
+            {
+                char[] specialChar = new char[] { '#', '@', '!', '$', '%', '^', '&', '*' };
+
+                foreach (char input in userInput)
+                {
+                    bool isInputValid = char.IsDigit(input) || 
+                                        char.IsLetter(input) || 
+                                        specialChar.Contains(input);
+
+                    if(isInputValid == true)
+                    {
+                        if (char.IsDigit(input))
+                        {
+                            displayNumber(input);
+                        }
+                        if (char.IsLetter(input))
+                        {
+                            displayLetter(input);
+                        }
+                        else if (specialChar.Contains(input))
+                        {
+                            displaySpecial(input);
+                        }
+                    }
+                    
+                    else
+                    {
+                        MessageBox.Show("Please enter a letter (A-Za-z), number (0-9), or special symbol (#@!$%^&*)");
+                        assignTenTextBox.Text = "";
+                    }
+                }
+            }
+
+        } //end assignTexTextBox
+
+        private void displayLetter(char letter)
+        {
+            //draws stick figure using the letter in input box
+            MessageBox.Show("      " + letter + letter + "  " +"\n"+
+                "    "+ letter +"    "+ letter+" "+"\n"+
+                "    " + letter + "    " + letter + " " + "\n" +
+                "      " + letter + letter + "  " + "\n"+
+                "       "+letter+"   "+"\n"+
+                "       "+letter+"   "+"\n"+
+                letter+ letter + letter + letter + letter + letter + letter + letter + "\n"+
+                "       " + letter + "\n" +
+                "       " + letter  + "\n" +
+                "       " + letter + "\n" +
+                "       " + letter + "\n"+
+                "     " + letter + "   " +letter+ "\n"+
+                "    " + letter + "     " + letter + "\n" +
+                "   " + letter + "       " + letter + "\n" +
+                "  " + letter + "         " + letter + "\n");
+
+        } //end displayLetter
+
+        private void displayNumber(char number)
+        {
+            int count = Convert.ToInt32(new string(number, 1));
+
+            //repeats "I love C#" according to number in text box
+            if (count > 0)
+            {
+                StringBuilder message = new StringBuilder();
+
+                for (int i = 1; i <= count; i++)
+                {
+                    message.Append("I love C# ");
+                }
+                MessageBox.Show(message.ToString());
+            }
+        } //end displayNumber
+
+        private void displaySpecial(char special)
+        {
+            //displays different message for each special character
+            switch (special){
+                case '@':
+                MessageBox.Show("Is that an eye staring at me?");
+                break;
+                case '!':
+                MessageBox.Show("Are you yelling at me?");
+                break;
+                case '#':
+                MessageBox.Show("In my day, we called that the pound sign.");
+                break;
+                case '$':
+                MessageBox.Show("Show me the money!!!!");
+                break;
+                case '%':
+                MessageBox.Show("60 percent of the time, this works every time.");
+                break;
+                case '^':
+                MessageBox.Show("The people's eyebrow.");
+                break;
+                case '&':
+                MessageBox.Show("....and another thing....");
+                break;
+                case '*':
+                MessageBox.Show("A star shines on the hour of our meeting");
+                break;
+
+            }
+
+        }//end displaySpecial
+    } //end Class
+} //end Namespace
